d002068392d9f9046dbc4cc244990f73fcae1157
航 遠藤 <w.endo@jc-21.jp>
2021-04-23 11:33:04 +0900

    Windowsプログラム終了

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,11 @@
 //インクルード
 #include <Windows.h>
 
+//定数宣言
+const char* WIN_CLASS_NAME = "SampleGame";  //ウィンドウクラス名
+const int CLIENT_WIDTH = 800;  //ウィンドウの幅
+const int CLIENT_HEIGHT = 600; //ウィンドウの高さ
+
 //プロトタイプ宣言
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -12,28 +17,34 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 
 	wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
 	wc.hInstance = hInstance;                   //インスタンスハンドル
-	wc.lpszClassName = "SampleGame";            //ウィンドウクラス名
+	wc.lpszClassName = WIN_CLASS_NAME;            //ウィンドウクラス名
 	wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
 	wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
+	wc.hIcon = LoadIcon(NULL, IDI_QUESTION); //アイコン
 	wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);   //小さいアイコン
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);   //マウスカーソル
+	wc.hCursor = LoadCursor(NULL, IDC_WAIT);   //マウスカーソル
 	wc.lpszMenuName = NULL;                     //メニュー（なし）
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
-	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //背景（白）
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH); //背景（白）
 
 	RegisterClassEx(&wc);  //クラスを登録
 
-	//ウィンドウを作成
+	//ウィンドウサイズの計算
+	RECT winRect = { 0, 0, CLIENT_WIDTH, CLIENT_HEIGHT };
+	AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE);
+	int winW = winRect.right - winRect.left;     //ウィンドウ幅
+	int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
+
+												 //ウィンドウを作成
 	HWND hWnd = CreateWindow(
-		"SampleGame",         //ウィンドウクラス名
+		WIN_CLASS_NAME,         //ウィンドウクラス名
 		"サンプルゲーム",     //タイトルバーに表示する内容
 		WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
 		CW_USEDEFAULT,       //表示位置左（おまかせ）
 		CW_USEDEFAULT,       //表示位置上（おまかせ）
-		800,                 //ウィンドウ幅
-		600,                 //ウィンドウ高さ
+		winW,                 //ウィンドウ幅
+		winH,                 //ウィンドウ高さ
 		NULL,                //親ウインドウ（なし）
 		NULL,                //メニュー（なし）
 		hInstance,           //インスタンス
@@ -75,6 +86,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
+
 	case WM_DESTROY:
 		PostQuitMessage(0);  //プログラム終了
 		return 0;
