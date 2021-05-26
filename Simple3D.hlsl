2930f291380354c2810315956d50860fd2698d8f
航 遠藤 <w.endo@jc-21.jp>
2021-05-26 10:12:44 +0900

    影ついたけど回らない

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -20,12 +20,13 @@ struct VS_OUT
 {
 	float4 pos  : SV_POSITION;	//位置
 	float2 uv	: TEXCOORD;		//UV座標
+	float4 color: COLOR;		//色（明るさ）
 };
 
 //───────────────────────────────────────
 // 頂点シェーダ
 //───────────────────────────────────────
-VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD)
+VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 {
 	//ピクセルシェーダーへ渡す情報
 	VS_OUT outData;
@@ -34,6 +35,11 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD)
 	//スクリーン座標に変換し、ピクセルシェーダーへ
 	outData.pos = mul(pos, matWVP);
 	outData.uv = uv;
+
+	float4 light = float4(0, 0, -1, 0);
+	light = normalize(light);
+	outData.color = dot(normal, light);
+
 
 	//まとめて出力
 	return outData;
@@ -44,5 +50,5 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD)
 //───────────────────────────────────────
 float4 PS(VS_OUT inData) : SV_Target
 {
-	return g_texture.Sample(g_sampler, inData.uv);
+	return g_texture.Sample(g_sampler, inData.uv) * inData.color;
 }
