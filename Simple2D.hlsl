bb3c692076227adf9be58aa63974388c73425b23
w.endo <w.endo@E0404-50>
2021-06-04 13:00:57 +0900

    アンビエント終わり、2Dに入ったとこ

--- Simple2D.hlsl
+++ Simple2D.hlsl
@@ -10,7 +10,6 @@ SamplerState	g_sampler : register(s0);	//サンプラー
 //───────────────────────────────────────
 cbuffer global
 {
-	float4x4	matWVP;			// ワールド・ビュー・プロジェクションの合成行列
 	float4x4	matW;			//ワールド行列
 };
 
@@ -21,28 +20,20 @@ struct VS_OUT
 {
 	float4 pos  : SV_POSITION;	//位置
 	float2 uv	: TEXCOORD;		//UV座標
-	float4 color	: COLOR;	//色（明るさ）
 };
 
 //───────────────────────────────────────
 // 頂点シェーダ
 //───────────────────────────────────────
-VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
+VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD)
 {
 	//ピクセルシェーダーへ渡す情報
 	VS_OUT outData;
 
-	//ローカル座標に、ワールド・ビュー・プロジェクション行列をかけて
+	//ローカル座標に、ワールド行列をかけて
 	//スクリーン座標に変換し、ピクセルシェーダーへ
-	outData.pos = mul(pos, matWVP);
+	outData.pos = mul(pos, matW);
 	outData.uv = uv;
-
-	//法線を回転
-	normal = mul(normal, matW);
-
-	float4 light = float4(-1, 1, -1, 0);
-	light = normalize(light);
-	outData.color = clamp(dot(normal, light), 0, 1);
 
 	//まとめて出力
 	return outData;
@@ -53,8 +44,5 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 //───────────────────────────────────────
 float4 PS(VS_OUT inData) : SV_Target
 {
-	float4 diffuse = g_texture.Sample(g_sampler, inData.uv) * inData.color;
-	float4 ambient = g_texture.Sample(g_sampler, inData.uv) * float4(0.2, 0.2, 0.2, 1);
-
-	return diffuse + ambient;
+	return  g_texture.Sample(g_sampler, inData.uv);
 }
